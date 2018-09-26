2f8a400ca211980bdb3a9bcc189dcef0e7fb6a17
ying.liu <yingliu@galaxys-MacBook-Pro.local>
2018-09-25 21:00:08 -0700

    fix aot bug
    fix readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
 
-import { initConfigServiceProviders } from 'ng-config-service'
-
-const configServiceProviders = [...initConfigServiceProviders()]
+import { bootConfigServiceProvider, NG_CONFIG_URL_TOKEN } from 'ng-config-service'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule],
-  providers: [configServiceProviders],
+  providers: [
+    {
+      provide: NG_CONFIG_URL_TOKEN,
+      useValue: 'assets/config/config2.json',
+    },
+    bootConfigServiceProvider(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
